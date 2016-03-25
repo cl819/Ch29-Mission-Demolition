be3466300512f243ce987fc75b5065eb08ecf7d4
Courtney Lam <cl819@scarletmail.rutgers.edu>
2016-03-24 21:06:29 -0400

    Levels

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -81,6 +81,7 @@ public class Slingshot : MonoBehaviour {
 			projectile.GetComponent<Rigidbody>().velocity = -mouseDelta * velocityMult;
 			FollowCam.S.poi = projectile;
 			projectile = null;
+			MissionDemolition.ShotFired ();
 		}
 	}
 }
